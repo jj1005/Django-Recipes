85b4e10bc9f4b2ddf3001ddb865d7d0093d77f9c
jj1005 <59148313+jj1005@users.noreply.github.com>
2023-12-21 00:01:29 +0200

    fixing lints

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,16 +8,15 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc  mosule."""
-    
+
     def test_add_numbers(self):
         """Test adding numbers togther."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
-        
